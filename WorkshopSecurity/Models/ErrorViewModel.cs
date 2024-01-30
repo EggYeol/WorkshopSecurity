bdd1397509c271707bce14340e464f5200609f89
Mintie | Tom Verra <tom.verra@hotmail.com>
2024-01-30 11:39:04 +0100

    Add project files.

---
+++ WorkshopSecurity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WorkshopSecurity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
